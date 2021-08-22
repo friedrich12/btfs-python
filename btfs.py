247cfc4935605101f3be502461c68ad547e1d2c6
Friedrich Doku <friedrichdoku@gmail.com>
2021-08-22 21:15:54 +0000

    Major changes to code

--- btfs.py
+++ btfs.py
@@ -6,6 +6,8 @@ try:
 except ImportError:
     pass
 import fuse
+import pycurl
+import StringIO
 from fuse import Fuse
 
 
@@ -54,6 +56,76 @@ class MyStat(fuse.Stat):
         self.st_mtime = 0
         self.st_ctime = 0
 
+class Read:
+    buf = None
+    index = None
+    offset = None
+    size = None
+    failed = False
+    parts = []
+
+    def __init__(self, b, idx , off, sz):
+        self.buf = b
+        self.index = idx
+        self.offset = off
+        self.size = sz
+        
+        ti = handle.torrent_file() 
+
+        file_size = ti.file_at(index).size;
+
+        while size > 0 and offset < file_size:
+            libtorrent.peer_request part = ti.map_file(index, offset, size)
+            part.length = min(ti.piece_size(part.piece) - part.start, part.length)
+            
+            parts.append(Part(part, buf))
+            self.size -= part.length;
+            self.offset += part.length;
+            self.buf += part.length
+    
+    def fail(piece):
+        for x in parts:
+            if x.part.piece == piece && not x.filled:
+                    self.failed = True
+    
+    #TODO: Check me later
+    def copy(piece, buffer, size):
+        for x in parts:
+            if x.part.piece == piece && not x.filled:
+                i.filled = buf[0:i.part.length] = buffer[i.part.start:]
+
+    def trigger():
+        for x in parts:
+            if handle.have_piece(x.part.piece):
+                handle.read_piece(x.part.piece)
+
+    def finished():
+        for x in parts:
+            if not i.filled:
+                return False
+        return True;
+
+    def size():
+        s = 0
+        for x in parts:
+            s += x.part.length
+        return s;
+
+    def read():
+        if self.size() <= 0:
+            return 0
+
+        # Trigger reads of finished pieces
+        self.trigger()
+
+        # Move sliding window to first piece to serve this request
+        self.jump(parts.front().part.piece, size())
+
+        if failed:
+            return -errno.EIO
+        else:
+            return self.size()
+
 class HelloFS(Fuse):
 
     def getattr(self, path):
@@ -79,6 +151,66 @@ class HelloFS(Fuse):
             stbuf->st_size = file_size
 
         return st
+    
+    def listxattr(path, data, len):
+        xattrs = None;               
+        xattrslen = 0;      
+ 
+        if (is_root(path)):           
+            xattrs = XATTR_IS_BTFS "\0" XATTR_IS_BTFS_ROOT
+            xattrslen = sizeof (XATTR_IS_BTFS "\0" XATTR_IS_BTFS_ROOT)
+        elif (is_dir(path)):               
+            xattrs = XATTR_IS_BTFS
+            xattrslen = sizeof (XATTR_IS_BTFS)
+        elif (is_file(path)):                                  
+            xattrs = XATTR_IS_BTFS "\0" XATTR_FILE_INDEX
+            xattrslen = sizeof (XATTR_IS_BTFS "\0" XATTR_FILE_INDEX)
+        else:                     
+            return -errno.ENOENT
+                                                                              
+        # The minimum required length
+        if len == 0:                                                 
+            return xattrslen
+                             
+        if len < xattrslen:        
+            return -ERANGE;                  
+                                       
+        data[:xattrslen] = xattrs
+
+        return xattrslen
+
+    def btfs_getxattr(path, key, value, len):
+        position = 0
+        xattr = [None] * 16
+        xattrlen = 0
+
+        k = str(key)
+
+        if is_file(path) and k == XATTR_FILE_INDEX:
+            xattrlen = snprintf(xattr, sizeof (xattr), "%d", files[path])
+        elif is_root(path) and k == XATTR_IS_BTFS_ROOT:
+            xattrlen = 0
+        elif k == XATTR_IS_BTFS:
+            xattrlen = 0
+        else:
+            return -errno.ENODATA
+
+        # The minimum required length
+        if len == 0:
+            return xattrlen
+
+        if position >= xattrlen:
+            return 0
+
+        if len <  xattrlen - position:
+            return -errno.ERANGE
+
+        
+        value[:xattrlen - position] = xattr[position:]
+        
+        return xattrlen -  position
+
+
 
     def readdir(self, path, buf, offset):
         if (not is_dir(path) and not is_file(path) and not is_root(path)):
@@ -115,18 +247,301 @@ class HelloFS(Fuse):
             return -errno.EISDIR  
                                       
                                                         
-        # Read *r = new Read(buf, files[path], offset, size);
+        r = Read(buf, files[path], offset, size)
                                   
-        # reads.push_back(r);
+        reads.append(r)
                                     
         # Wait for read to finish             
-        # int s = r->read();                    
+        s = r.read()                    
                        
-        # reads.remove(r);
+        reads.remove(r);
                                  
-        # delete r;                                            
-                                                                    
-        # return s; 
+        return s
+
+def handle_read_piece_alert(a):
+    if a.ec:
+	print(a.message())
+
+        for x in reads:
+            x.fail(a.piece)
+    else:
+        for x in reads:
+            x.copy(a.piece, a.buffer.get(), a.size())
+
+def handle_piece_finished_alert(a):
+    #printf("%s: %d\n", __func__, static_cast<int>(a->piece_index));
+
+    for x in reads:
+        x.trigger()
+
+    # Advance sliding window
+    advance()
+
+def handle_torrent_added_alert(a):
+    handle = a.handle
+
+    if a.handle.status().has_metadata:
+        setup()
+
+def handle_metadata_received_alert(a):
+    handle = a.handle
+    setup()
+
+def handle_alert(a):
+    if a.type == libtorrent.read_piece_alert.alert_type:
+        handle_read_piece_alert(a)
+    elif a.type == libtorrent.piece_finished_alert.alert_type:
+        handle_piece_finished_alert(a)
+    elif a.type == libtorrent.metadata_received_alert.alert_type:
+        handle_metadata_received_alert(a)
+    elif a.type == libtorrent.torrent_added_alert.alert_type:
+        handle_torrent_added_alert(a)
+    elif a.type == libtorrent.dht_bootstrap_alert.alert_type:
+        # Force DHT announce because libtorrent won't by itself
+        handle.force_dht_announce()
+    else:
+        print(a.message())
+
+def setup():
+    print("Got metadata. Now ready to start downloading.\n")
+
+    ti = handle.torrent_file()
+
+    if params.browse_only:
+        handle.pause()
+
+    for (int i = 0; i < ti->num_files(); ++i):
+        parent = ""
+        p = str(ti.file_at(i).path.c_str())
+
+        if not p:
+            continue
+        for (char *x = strtok(p, "/"); x; x = strtok(NULL, "/")):
+            if len(x) <= 0:
+                continue
+
+            if parent.length() <= 0:
+                # Root dir <-> children mapping
+                dirs["/"] = x
+	    else:
+                # Non-root dir <-> children mapping
+		dirs[parent] = x
+
+                parent += "/"
+                parent += x
+		
+
+		free(p);
+
+		# Path <-> file index mapping
+		files["/" + ti->file_at(i).path] = i
+
+def alert_queue_loop_destroy(data):
+    #Log *log = (Log *) data;
+    #if (log):
+    #    delete log
+
+
+def aert_queue_loop(data):
+    while True:
+        if not session.wait_for_alert(libtorrent.seconds(1)):
+            continue
+
+        alerts = []
+        session.pop_alerts(&alerts)
+
+        for x in alerts:
+            handle_alert(x)
+
+def btfs_statfs(path, stbuf):
+
+    if not handle.is_valid():
+        return -errno.ENOENT
+
+    st = handle.status();
+
+    if not st.has_metadata:
+        return -errno.ENOENT
+
+    ti = handle.torrent_file()
+
+    stbuf.f_bsize = 4096
+    stbuf.f_frsize = 512
+    stbuf.f_blocks =  (ti->total_size() / 512)
+    stbuf.f_bfree =  ((ti->total_size() - st.total_done) / 512)
+    stbuf.f_bavail =  ((ti->total_size() - st.total_done) / 512)
+    stbuf.f_files =  (files.size() + dirs.size());
+    stbuf.f_ffree = 0;
+
+
+def btfs_init(conn):
+
+    time_of_mount = None
+
+    p = fuse_get_context()->private_data
+
+    flags = libtorrent.session.add_default_plugins | libtorrent.session.start_default_features
+
+    alerts = libtorrent.alert.tracker_notification |
+		libtorrent.alert.stats_notification |
+		libtorrent.alert.storage_notification |
+		libtorrent.alert.progress_notification |
+		libtorrent.alert.status_notification |
+		libtorrent.alert.error_notification |
+		libtorrent.alert.dht_notification |
+		libtorrent.alert.peer_notification
+
+    session = libtorrent.session(
+		libtorrent.fingerprint(
+			"LT",
+			LIBTORRENT_VERSION_MAJOR,
+			LIBTORRENT_VERSION_MINOR,
+			0,
+			0),
+		(params.min_port, params.max_port),
+		"0.0.0.0",
+		flags,
+		alerts)
+
+    se = session.settings()
+
+    se.request_timeout = 10
+    se.strict_end_game_mode = False
+    se.announce_to_all_trackers = True
+    se.announce_to_all_tiers = True
+    se.enable_incoming_tcp = not params.utp_only
+    se.enable_outgoing_tcp = not params.utp_only
+    se.download_rate_limit = params.max_download_rate * 1024
+    se.upload_rate_limit = params.max_upload_rate * 1024
+
+    session.set_settings(se)
+    session.add_dht_router(("router.bittorrent.com", 6881))
+    session.add_dht_router(("router.utorrent.com", 6881))
+    session.add_dht_router(("dht.transmissionbt.com", 6881))
+    session.async_add_torrent(p)
+
+    pack.set_int(pack.request_timeout, 10)
+    pack.set_str(pack.listen_interfaces, interfaces.str())
+    pack.set_bool(pack.strict_end_game_mode, false)
+    pack.set_bool(pack.announce_to_all_trackers, true)
+    pack.set_bool(pack.announce_to_all_tiers, true)
+    pack.set_bool(pack.enable_incoming_tcp, not params.utp_only)
+    pack.set_bool(pack.enable_outgoing_tcp, not params.utp_only)
+    pack.set_int(pack.download_rate_limit, params.max_download_rate * 1024)
+    pack.set_int(pack.upload_rate_limit, params.max_upload_rate * 1024)
+    pack.set_int(pack.alert_mask, alerts)
+
+    session = libtorrent.session(pack, flags)
+
+    session.add_torrent(p)
+
+
+
+def btfs_destroy(user_data):
+    flags = 0
+
+    if not params.keep:
+        flags |= libtorrent.session.delete_files
+
+    session.remove_torrent(handle, flags)
+
+
+def populate_target(target, arg):
+    templ = str()
+
+    if arg:
+        templ += arg;
+    elif os.getenv("XDG_DATA_HOME"):
+        templ += os.getenv("XDG_DATA_HOME")
+        templ += "/btfs";
+    elif (getenv("HOME")):
+        templ += getenv("HOME")
+        templ += "/btfs"
+    else:
+        templ += "/tmp/btfs"
+
+    if os.mkdir(templ.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) < 0:
+        if errno != errno.EEXIST:
+            RETV(perror("Failed to create target"), False)
+
+	templ += "/btfs-XXXXXX";
+
+	s = str(templ.c_str())
+
+        if s != None and mkdtemp(s) != None:
+            x = realpath(s, None)
+
+            if x:
+                target = x
+            else:
+                print("FAILED TO EXPAND TARGET")
+
+        else:
+		print("Failed to generate target")
+
+
+        return target.length() > 0
+
+def handle_http(contents, size, nmemb, userp):
+    output = userp
+
+    # Offset into buffer to write to
+    off = output.size
+
+    output.expand(nmemb * size)
+
+    output.buf[off:nmeb*size] = contents
+
+    # Must return number of bytes copied
+    return nmemb * size
+
+def populate_metadata(p, arg):
+    uri = str(arg)
+
+    if uri.find("http:") == 0 or uri.find("https:") == 0:
+        output = []
+        c = pycurl.Curl()
+        c.setopt(c.URL, uri)
+        c.setopt(c.WRITEFUNCTION, handle_http)
+        c.setopt(c.WRITEDATA, output)
+        c.setopt(c.USERAGENT, "btfs/" VERSION)
+        c.setopt(c.POSTFIELDS, '@request.json')
+        c.setopt(c.CURLOPT_FOLLOWLOCATION, 1)
+        c.perform()
+        c.close()      
+
+
+	ec = None 
+
+	p.ti = libtorrent.torrent_info(output.buf, output.size, ec)
+
+        if ec:
+            if params.browse_only:
+                p.flags |= libtorrent.add_torrent_params.flag_paused
+
+        elif uri.find("magnet:") == 0:
+            ec = None
+            parse_magnet_uri(uri, p, ec)
+
+            if ec:
+                print("Failed to parse magnet\n")
+        else:
+		r = os.path.realpath(uri.c_str())
+
+		if not r:
+                    print("Find metadata failed")
+
+		ec = None
+
+		p.ti = libtorrent.torrent_info(r, ec)
+
+                if ec:
+                    print("Parse metadata failed: %s\n")
+
+                if params.browse_only:
+			p.flags |= libtorrent.add_torrent_params.flag_paused
+
+	return true
 
 def main():
     usage="""
